7d0bb9ae735c85cc5b165ba64078d89f4d1489b1
erikdrewdunning <erikdunning@icloud.com>
2021-11-17 12:55:45 -0700

    Add Contact Repo and some form validation

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -3,6 +3,7 @@ package com.example.application.views.list;
 import java.util.Collections;
 
 import com.example.application.data.entity.Contact;
+import com.example.application.data.service.CrmService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
@@ -21,14 +22,18 @@ public class ListView extends VerticalLayout {
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
     ContactForm form; 
+    CrmService service;
 
-    public ListView() {
+
+    public ListView(CrmService service) {
+    	this.service=service;
         addClassName("list-view");
         setSizeFull();
         configureGrid();
         configureForm(); 
 
         add(getToolbar(), getContent()); 
+        updateList();
     }
     
     private Component getContent() {
@@ -57,12 +62,17 @@ public class ListView extends VerticalLayout {
     private HorizontalLayout getToolbar() {
         filterText.setPlaceholder("Filter by name...");
         filterText.setClearButtonVisible(true);
-        filterText.setValueChangeMode(ValueChangeMode.LAZY); 
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList()); 
+
 
         Button addContactButton = new Button("Add contact");
 
         HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton); 
         toolbar.addClassName("toolbar");
         return toolbar;
+    }
+    private void updateList() { 
+        grid.setItems(service.findAllContacts(filterText.getValue()));
     }
 }
