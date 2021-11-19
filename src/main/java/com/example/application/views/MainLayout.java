a9517db089f5886d5b43283a3eb75fd5caee8e04
erikdrewdunning <erikdunning@icloud.com>
2021-11-19 14:29:43 -0700

    Add login screen

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -1,21 +1,24 @@
 package com.example.application.views;
 
+import com.example.application.security.SecurityService;
 import com.example.application.views.list.ListView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.HighlightConditions;
 import com.vaadin.flow.router.RouterLink;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 
-@Theme(themeFolder = "flowcrmtutorial") 
-public class MainLayout extends AppLayout { 
+@Theme(themeFolder = "flowcrmtutorial")
+public class MainLayout extends AppLayout {
+    private final SecurityService securityService;
 
-    public MainLayout() {
+    public MainLayout(SecurityService securityService) { 
+        this.securityService = securityService;
         createHeader();
         createDrawer();
     }
@@ -24,21 +27,21 @@ public class MainLayout extends AppLayout {
         H1 logo = new H1("Vaadin CRM");
         logo.addClassNames("text-l", "m-m");
 
-        HorizontalLayout header = new HorizontalLayout(
-          new DrawerToggle(), 
-          logo
-        );
+        Button logout = new Button("Log out", e -> securityService.logout()); 
 
-        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER); 
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo, logout); 
+
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.expand(logo); 
         header.setWidth("100%");
         header.addClassNames("py-0", "px-m");
 
-        addToNavbar(header); 
+        addToNavbar(header);
 
     }
 
     private void createDrawer() {
-    	RouterLink listLink = new RouterLink("List", ListView.class);
+        RouterLink listLink = new RouterLink("List", ListView.class);
         listLink.setHighlightCondition(HighlightConditions.sameLocation());
 
         addToDrawer(new VerticalLayout(
